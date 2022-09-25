3ab48acf93118c05295c66b82d736e1885a0e63e
Leon W <31262614+leanleon93@users.noreply.github.com>
2022-09-25 23:48:17 +0200

    add examples

---
+++ LostArkPatcher/Examples/enable_guardian_hp_bars.sql
@@ -0,0 +1,4 @@
+--File:EFTable_Npc.db
+UPDATE Npc
+SET NameTag = 1, TargetUI = 1, Grade = 6
+Where Grade = 9 or Grade = 7;
---
+++ LostArkPatcher/Examples/outfitswaps.sql
@@ -0,0 +1,10 @@
+--File:EFTable_Item.db
+UPDATE Item
+SET Model = 'EFDLItem_PC_GDH_F_HR_03_Upper.PC_GDH_F_HR_03_Upper'
+WHERE PrimaryKey = 325600323;
+UPDATE Item
+Set Model = 'EFDLItem_PC_GDH_F_HR_03_Lower.PC_GDH_F_HR_03_Lower'
+WHERE PrimaryKey = 335600323;
+UPDATE Item
+Set Model = 'EFDLItem_PC_MBD_HR_00A-1_Lower.PC_MBD_HR_00A-1_Lower'
+WHERE PrimaryKey = 332090130;
