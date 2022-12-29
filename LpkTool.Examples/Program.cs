4b8e909043da7455a69db51e814b68dbafc7f79f
Leon W <31262614+leanleon93@users.noreply.github.com>
2022-12-29 07:15:18 +0100

    no top-level statements

--- LpkTool.Examples/Program.cs
+++ LpkTool.Examples/Program.cs
@@ -1,39 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Testing Project
 
-
-//Testing Project
-
-//ModelSwap.Swaps();
-using LpkTool.Examples;
 using LpkTool.Library;
 using Microsoft.Win32;
 
-//ModelSwap.Dumps();
 
-var newFontPath = @"C:\Users\leanw\Documents\LostArk\patches\RobotoCondensed-Regular.ttf";
-
-var lostArkInstallDir = (string?)Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App 1599340", "InstallLocation", "");
-var efGamePath = Path.Combine(lostArkInstallDir, "EFGame");
-
-var baseName = "data{0}.lpk";
-var outDir = Path.Combine(efGamePath, "out");
-var fontPath = Path.Combine(efGamePath, "font.lpk");
-
-
-//FontChange.RepackWithChangedFont(fontPath, newFontPath);
-
-Console.WriteLine("Unpacking font");
-var lpk1 = Lpk.FromFile(fontPath);
-lpk1.Export(outDir);
-
-
-
-for (int i = 1; i <= 4; i++)
+internal class Program
 {
-    Console.WriteLine("Unpacking " + i);
-    var filePath = Path.Combine(efGamePath, string.Format(baseName, i));
-    var lpk = Lpk.FromFile(filePath);
-    lpk.Export(outDir);
-}
+    private static void Main(string[] args)
+    {
+        var lostArkInstallDir = (string?)Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App 1599340", "InstallLocation", "");
+        var efGamePath = Path.Combine(lostArkInstallDir, "EFGame");
 
-//FontChange.RepackWithRoboto(@"F:\SteamLibrary\steamapps\common\Lost Ark\EFGame\font.lpk", @"F:\SteamLibrary\steamapps\common\Lost Ark\EFGame\Roboto-Regular.ttf", @"F:\SteamLibrary\steamapps\common\Lost Ark\EFGame\FontMap.xml");
+        var baseName = "data{0}.lpk";
+        var outDir = Path.Combine(efGamePath, "out");
+        var fontPath = Path.Combine(efGamePath, "font.lpk");
+
+        Console.WriteLine("Unpacking font");
+        var lpk1 = Lpk.FromFile(fontPath);
+        lpk1.Export(outDir);
+
+        for (var i = 1; i <= 4; i++)
+        {
+            Console.WriteLine("Unpacking " + i);
+            var filePath = Path.Combine(efGamePath, string.Format(baseName, i));
+            var lpk = Lpk.FromFile(filePath);
+            lpk.Export(outDir);
+        }
+    }
+}
