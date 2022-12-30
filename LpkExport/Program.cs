eb4a94997ceed47898a374865b52d6f95b134ce5
Leon W <31262614+leanleon93@users.noreply.github.com>
2022-12-30 06:50:32 +0100

    add simple lpk export project

---
+++ LpkExport/Program.cs
@@ -0,0 +1,31 @@
+﻿using LpkTool.Library;
+
+namespace LpkExport
+{
+    internal class Program
+    {
+        private static readonly string _usage = "Usage LpkExport.exe \"data.lpk\"";
+        private static readonly string _exporting = "Exporting \"{0}\"";
+
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine(_usage);
+                return;
+            }
+            var fileArg = args[0];
+            if (!fileArg.EndsWith(".lpk") || !File.Exists(fileArg))
+            {
+                Console.WriteLine(_usage);
+                return;
+            }
+            var lpk = Lpk.FromFile(fileArg);
+            var dir = Path.GetDirectoryName(fileArg);
+            var filename = Path.GetFileName(fileArg);
+            dir = Path.Combine(dir!, filename + ".files");
+            Console.WriteLine(string.Format(_exporting, filename));
+            lpk.Export(dir);
+        }
+    }
+}
